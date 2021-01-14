520e6d54ab703fed5a4ca82d730b2617608561af
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2021-01-13 20:28:17 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const morgan = require('morgan');
+const path = require('path'); 
+const app = express(); 
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//midleware
+app.use(morgan('dev'));
+app.use(express.json())
+
+//Routes
+app.use('/api/tasks', require('./routes/task.routes.js'))
+
+//Static files
+console.log('path = ' + path.join(__dirname, 'public'));
+
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log("server on port " + app.get('port'));
+})
